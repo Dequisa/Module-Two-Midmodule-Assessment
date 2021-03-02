8bc8f199248dd8e86a41f0f5d827a45611c1410b
Dequisa <Dequisas@gmail.com>
2021-03-02 12:41:35 -0500

    SMART changes

--- style.js
+++ style.js
@@ -1,21 +1,18 @@
+// SMART Goal- Reduce the number of arrays being used.
 console.log("This is Your Dice Game")
+const dice = ['&#9856;','&#9857;','&#9858;','&#9859;','&#9860;','&#9861;']
 
 document.addEventListener("DOMContentLoaded",()=>{
-    const dice = ['&#9856;','&#9857;','&#9858;','&#9859;','&#9860;','&#9861;']
 
 
-    let form = document.querySelector("form")
-    form.addEventListener("submit", (event)=>{
-        event.preventDefault()
 
 let rollButton = document.querySelector('#start-game')
         let num = 0;
-        let array = []
+        // let array = []
         let totSum = [];
         
     rollButton.addEventListener("click",(e)=>{
         
-       
         e.preventDefault()
         let para = document.getElementById("dice")
         let diceDiv = document.getElementById("dice-para")
@@ -29,62 +26,42 @@ let rollButton = document.querySelector('#start-game')
       let random = Math.floor(Math.random()* 6) 
     
         if(random ===1){
-            // para.innerHTML+= dice[0]
             totSum.push(dice[0])
             num +=1
-            array.push(dice[0])
+            // array.push(dice[0])
             }else if(random ===2){
-                // para.innerHTML+= dice[1]
                 totSum.push(dice[1]) 
                 num +=2
-                array.push(dice[1])
-            }else if(random ===3){
-                // para.innerHTML+= dice[2] 
+                // array.push(dice[1])
+            }else if(random ===3){ 
                 totSum.push(dice[2]) 
                 num +=3
-                array.push(dice[2])
+                // array.push(dice[2])
             }else if(random ===4){
-                // para.innerHTML+= dice[3]
                 totSum.push(dice[3])  
                 num +=4
-                array.push(dice[3])
+                // array.push(dice[3])
             }else if(random ===5){
-                // para.innerHTML+= dice[4]
                 totSum.push(dice[4])  
                 num +=5
-                array.push(dice[4])
+                // array.push(dice[4])
             }else{
-                // para.innerHTML+= dice[5]
                 totSum.push(dice[5])  
                 num +=6 
-                array.push(dice[5])
+                // array.push(dice[5])
                 }
-        // sum.textContent = `Sum = ${num}`;
-        
             }
             sum.textContent = `Sum = ${num}`;
             para.innerHTML=" "
-            totSum.map((e)=>{para.innerHTML += e })
+            totSum.map((e)=>{para.innerHTML += e;
+                createLi.innerHTML += e })
             diceDiv.appendChild(para)
-            para.parentNode.replaceChild(para, para)
-            // diceDiv.appendChild(para)
-            console.log(diceDiv)
-            console.log(para.innerHTML)
-             array.map((e)=>{createLi.innerHTML += e })
+            // para.parentNode.replaceChild(para, para)
+            //  array.map((e)=>{createLi.innerHTML += e })
         createLi.innerHTML += `=${num}`
         history.appendChild(createLi)
-        array=[]
+        // array=[]
         totSum=[]
         num=0;
-        
-           
-
-    })
-    
-    
-    
-   
-       
-
-})
+    })     
 })
